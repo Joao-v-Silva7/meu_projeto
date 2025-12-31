92708bd2904713474844d4bdc34eca737c2acba8
Joao-v-Silva7 <jv2806633@gmail.com>
2025-12-31 13:35:27 -0300

    feat: validando os Dtos do task

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,32 @@
+import { IsUUID, IsString, MinLength, IsEnum, IsOptional, IsDateString, MaxLength } from "class-validator";
+
+
+export enum TaskStatusEnum {
+    TO_DO = 'TO_DO',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE'
+}
+
 export class TaskDto {
+    @IsUUID()
+    @IsOptional()
     id: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(255)
     tittle: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(500)
     description: string;
+
+    @IsEnum(TaskStatusEnum)
+    @IsOptional()
     status: string;
+
+    @IsDateString()
     expirationDate: Date;
 }
 
